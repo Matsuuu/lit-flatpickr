0fd9e5d752060e7491a8eea53691fa99958c900c
Matsuuu <huhta.matias@gmail.com>
2021-03-03 12:20:05 +0200

    Update example

--- demo/custom-input.js
+++ demo/custom-input.js
@@ -1,11 +1,11 @@
 import { LitElement, html } from 'lit-element';
 
 export default class CustomInput extends LitElement {
-  render() {
-    return html`<div><input /></div>`;
-  }
+    render() {
+        return html`<div><input placeholder="dd/mm/yyyy" /></div>`;
+    }
 }
 
 if (!customElements.get('custom-input')) {
-  customElements.define('custom-input', CustomInput);
+    customElements.define('custom-input', CustomInput);
 }
