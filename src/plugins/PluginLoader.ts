1d668ce61c84b8419fe0812300788001cbd2ff4f
Matsuuu <huhta.matias@gmail.com>
2022-05-12 20:05:15 +0300

    Month Select Plugin

--- src/plugins/PluginLoader.ts
+++ src/plugins/PluginLoader.ts
@@ -1,9 +1,11 @@
 import { LitFlatpickr } from '../LitFlatpickr';
 
+const pluginUrlPrefix = 'https://npmcdn.com/flatpickr@4.6.9/dist';
+
 export async function loadPlugins(instance: LitFlatpickr, options: any) {
   if (instance.weekSelect) {
     // @ts-ignore
-    const weekSelectPluginImport = await import('flatpickr/dist/esm/plugins/weekSelect/weekSelect.js');
+    const weekSelectPluginImport = await import(pluginUrlPrefix + '/esm/plugins/weekSelect/weekSelect.js');
     const weekSelectPlugin = weekSelectPluginImport.default;
     options = {
       ...options,
@@ -13,6 +15,27 @@ export async function loadPlugins(instance: LitFlatpickr, options: any) {
         this.input.value = weekNumber;
       },
     };
+  }
+
+  if (instance.monthSelect) {
+    // @ts-ignore
+    const monthSelectPluginImport = await import(pluginUrlPrefix + '/esm/plugins/monthSelect/index.js');
+    const monthSelectPlugin = monthSelectPluginImport.default;
+    options = {
+      ...options,
+      plugins: [
+        ...options.plugins,
+        monthSelectPlugin({
+          shorthand: false,
+          dateFormat: instance.dateFormat,
+          altFormat: instance.altFormat,
+        }),
+      ],
+    };
+    const styles = document.createElement('link');
+    styles.rel = 'stylesheet';
+    styles.href = pluginUrlPrefix + '/plugins/monthSelect/style.css';
+    document.head.appendChild(styles);
   }
 
   return options;
